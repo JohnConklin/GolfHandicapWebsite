35fcca6623a6d68d118a34cff0e78461e8cd7a80
John Conklin <john.conklin@conklin.phxcoxmail.com>
2017-12-30 09:26:15 -0700

    test

--- GolfHandicapCalculator/Startup.cs
+++ GolfHandicapCalculator/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using GolfHandicapCalculator.Models;
+using System.IO;
 
 namespace GolfHandicapCalculator
 {
@@ -37,6 +38,23 @@ namespace GolfHandicapCalculator
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            //adding to avoid using "api/values" on startup.
+            app.Use(async (context, next) =>
+            {
+                await next();
+
+                if (context.Response.StatusCode == 404 && !Path.HasExtension(context.Request.Path.Value) && !context.Request.Path.Value.StartsWith("api"))
+                {
+                    context.Request.Path = "/Index.html";
+                    context.Response.StatusCode = 200;
+                    await next();
+                }
+            });
+
+            //Added to avoid using api/values on startup
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
 
             app.UseCors(p => p.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader().AllowCredentials());
 
