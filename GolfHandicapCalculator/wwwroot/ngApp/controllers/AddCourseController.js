7b1145f041834765f13b670752cae7ad3b6c4002
John Conklin <john.conklin@conklin.phxcoxmail.com>
2018-01-11 18:19:03 -0700

    More updates

---
+++ GolfHandicapCalculator/wwwroot/ngApp/controllers/AddCourseController.js
@@ -0,0 +1,27 @@
+﻿//list existing courses
+
+export class ListCourseController {
+
+    constructor(courseService) {
+        this.course = courseService.listCourses();
+    }
+}
+
+ListCourseController.$inject = ['listCourses'];
+
+//add new course
+export class CourseAddController {
+
+    constructor(courseService, $state) {
+        this.courseService = courseService;
+        this.$state = $state;
+    }
+
+    addCourse() {
+        this.courseService.save(this.course).then(
+            () => this.$state.go('add')
+        );
+    }
+}
+
+ListCourseController.$inject = ['courseService', '$state'];
--- GolfHandicapCalculator/wwwroot/ngApp/services/services.js
+++ GolfHandicapCalculator/wwwroot/ngApp/services/services.js
@@ -1,6 +1,28 @@
-﻿class AccountService {
+﻿export class AccountService {
     isLoggedIn() {
         return false;
 
-    };
+    }
 }
+
+export class CourseService {
+
+    constructor($resource) {
+        this.CourseService = $resource('/api/addcourse/:id');
+    }
+
+    listCourses() {
+        return this.CourseService.query();
+    }
+
+    save(course) {
+        return this.CourseService.save(course).$promise;
+    }
+
+    getCourse(id) {
+        return this.CourseService.get({ id: id });
+    }
+
+}
+
+CourseService.$inject = ['$resource'];
